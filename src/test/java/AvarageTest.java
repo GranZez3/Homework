67cb11c0fedf073a5235ebcee7d176ae85583563
erbol <smth.@gmail.com>
2025-11-11 00:11:26 +0600

    Second commit

---
+++ src/test/java/AvarageTest.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import tasks.Avarage;
+
+public class AvarageTest{
+    @Test
+    void testAvarage(){
+        Avarage avarage = new Avarage();
+    }
+}
